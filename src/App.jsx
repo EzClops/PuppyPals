5fffddb710d126483e3891caada88c72ba20847c
Jeramiha Holt <jeramihaholt04@gmail.com>
2023-06-15 13:34:34 -0700

    added more puppy data

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,31 @@ import { useState } from 'react'
 function App() {
   // console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   // console.log(puppies);
+
+  function handleClick() {
+
+  }
+
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  console.log(featuredPup);
+
 
   return (
     <>
+      <p>{ featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      ) }</p>
       {
         puppies.map(puppy => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
         })
       }
         
